a0da0949924d38156b3e0bff8c02ffceea6e610d
quokka <ansagi3@naver.com>
2025-03-20 03:14:31 +0900

    ✔ 스타터팩 구성 완료

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // 필요한 경로 추가
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
