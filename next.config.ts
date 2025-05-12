2ebb27d2b0482fc4f56f6c35c80a2bb45c615374
quokka <ansagi3@naver.com>
2025-05-13 04:23:45 +0900

    next.config.ts 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,21 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-// import type { NextConfig } from "next";
-
-// const storageUrl = process.env.NEXT_PUBLIC_SUPABASE_STORAGE;
-// const storageDomain = storageUrl ? new URL(storageUrl).host : "";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     domains: storageDomain ? [storageDomain] : [],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const storageUrl = process.env.NEXT_PUBLIC_SUPABASE_STORAGE;
 const storageDomain = storageUrl ? new URL(storageUrl).host : "";
 
 const nextConfig: NextConfig = {
-  images: storageDomain
-    ? {
-        remotePatterns: [
+  images: {
+    remotePatterns: storageDomain
+      ? [
           {
-            protocol: "https", // 항상 https로 설정
+            protocol: "https",
             hostname: storageDomain,
+            port: "",
+            pathname: "/storage/v1/object/public/blog-images/**",
           },
-        ],
-      }
-    : {}, // storageDomain이 없으면 remotePatterns 설정을 아예 하지 않음
+        ]
+      : [],
+  },
 };
 
 export default nextConfig;
